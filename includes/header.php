310e2ecc9665d6e38bdc16ae9e4ea6b7c7f2f91d
Anant Singh <anant.singh@copters.in>
2026-06-26 12:41:46 +0530

    HTML Changes

--- includes/header.php
+++ includes/header.php
@@ -4,19 +4,21 @@ require_once __DIR__ . '/config.php';
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title><?php echo APP_NAME; ?></title>
-</head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../public/assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../public/assets/css/style.css">
+</head>
+
 <body>
 
-<h1><?php echo APP_NAME; ?></h1>
+    <h1><?php echo APP_NAME; ?></h1>
 
-<nav>
-    <a href="index.php">Search</a> |
-    <a href="upload.php">Upload Excel</a>
-</nav>
+    <nav>
+        <a href="index.php">Search</a> |
+        <a href="upload.php">Upload Excel</a>
+    </nav>
